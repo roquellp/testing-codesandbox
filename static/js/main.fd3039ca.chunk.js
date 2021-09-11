74049c9481365d3ef8ef6b93232cdb7300d70111
roquellp <roque.llp@gmail.com>
2021-09-10 21:44:16 -0300

    Updates

---
+++ static/js/main.fd3039ca.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptesting-sandbox"]=this["webpackJsonptesting-sandbox"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var s=n(1),c=n.n(s),i=n(3),r=n.n(i),o=(n(8),n(0));var a=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"Aloha"})})};r.a.render(Object(o.jsx)(c.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.fd3039ca.chunk.js.map
